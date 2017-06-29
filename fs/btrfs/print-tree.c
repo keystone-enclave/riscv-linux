435a41686c772bc35d6c63e451a3d826fa9f4b9a
David Sterba <dsterba@suse.com>
2017-06-29 18:37:49 +0200

    btrfs: get fs_info from eb in btrfs_print_tree, remove argument
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/print-tree.c
+++ fs/btrfs/print-tree.c
@@ -319,8 +319,9 @@ void btrfs_print_leaf(struct extent_buffer *l)
 	}
 }
 
-void btrfs_print_tree(struct btrfs_fs_info *fs_info, struct extent_buffer *c)
+void btrfs_print_tree(struct extent_buffer *c)
 {
+	struct btrfs_fs_info *fs_info = c->fs_info;
 	int i; u32 nr;
 	struct btrfs_key key;
 	int level;
@@ -360,7 +361,7 @@ void btrfs_print_tree(struct btrfs_fs_info *fs_info, struct extent_buffer *c)
 		if (btrfs_header_level(next) !=
 		       level - 1)
 			BUG();
-		btrfs_print_tree(fs_info, next);
+		btrfs_print_tree(next);
 		free_extent_buffer(next);
 	}
 }
