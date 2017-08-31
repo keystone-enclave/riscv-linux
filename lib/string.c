92bf53c554cc7ed08f145e0dc000b4c73b86b4ab
Andrew Morton <akpm@linux-foundation.org>
2017-09-01 09:49:32 +1000

    add-testcases-for-memset16-32-64-fix
    minor tweaks
    
    Cc: Matthew Wilcox <mawilcox@microsoft.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: "James E.J. Bottomley" <jejb@linux.vnet.ibm.com>
    Cc: "Martin K. Petersen" <martin.petersen@oracle.com>
    Cc: David Miller <davem@davemloft.net>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Ivan Kokshaysky <ink@jurassic.park.msu.ru>
    Cc: Matt Turner <mattst88@gmail.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Minchan Kim <minchan@kernel.org>
    Cc: Ralf Baechle <ralf@linux-mips.org>
    Cc: Richard Henderson <rth@twiddle.net>
    Cc: Russell King <rmk+kernel@armlinux.org.uk>
    Cc: Sam Ravnborg <sam@ravnborg.org>
    Cc: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- lib/string.c
+++ lib/string.c
@@ -1150,7 +1150,6 @@ fail:
 	if (i < 256)
 		return (i << 24) | (j << 16) | k;
 	return 0;
-	return 0;
 }
 
 static __init int string_selftest_init(void)
@@ -1180,4 +1179,4 @@ fail:
 }
 
 module_init(string_selftest_init);
-#endif
+#endif	/* CONFIG_STRING_SELFTEST */
