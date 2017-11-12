14f276385bb2d14948c14a9dbdad157231d95c91
Andi Kleen <ak@linux.intel.com>
2017-11-13 07:53:48 +1100

    support-resetting-warn_once-for-all-architectures-v2
    add a missing ifdef CONFIG_MODULES. Thanks 0day!
    
    Link: http://lkml.kernel.org/r/20171020170633.9593-1-andi@firstfloor.org
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- lib/bug.c
+++ lib/bug.c
@@ -209,11 +209,13 @@ void generic_bug_clear_once(void)
 {
 	struct module *mod;
 
+#ifdef CONFIG_MODULES
 	rcu_read_lock_sched();
 	list_for_each_entry_rcu(mod, &module_bug_list, bug_list)
 		clear_once_table(mod->bug_table,
 				 mod->bug_table + mod->num_bugs);
 	rcu_read_unlock_sched();
+#endif
 
 	clear_once_table(__start___bug_table, __stop___bug_table);
 }
