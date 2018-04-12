5e64a631215597bd86b16b7d1d09ba50625ca5da
Anand Jain <anand.jain@oracle.com>
2018-04-12 10:29:34 +0800

    btrfs: drop uuid_mutex in btrfs_open_devices()
    btrfs_open_devices() is using uuid_mutex, but as the btrfs_open_devices()
    is just limited to open all the devices under a given fsid, so we don't
    need uuid_mutex.
    
    Instead it should hold the device_list_mutex as it updated the status
    of the btrfs_fs_devices and btrfs_device of a fsid.
    
    Signed-off-by: Anand Jain <anand.jain@oracle.com>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/volumes.c
+++ fs/btrfs/volumes.c
@@ -1110,7 +1110,7 @@ int btrfs_open_devices(struct btrfs_fs_devices *fs_devices,
 {
 	int ret;
 
-	mutex_lock(&uuid_mutex);
+	mutex_lock(&fs_devices->device_list_mutex);
 	if (fs_devices->opened) {
 		fs_devices->opened++;
 		ret = 0;
@@ -1118,7 +1118,7 @@ int btrfs_open_devices(struct btrfs_fs_devices *fs_devices,
 		list_sort(NULL, &fs_devices->devices, devid_cmp);
 		ret = open_fs_devices(fs_devices, flags, holder);
 	}
-	mutex_unlock(&uuid_mutex);
+	mutex_unlock(&fs_devices->device_list_mutex);
 	return ret;
 }
 
