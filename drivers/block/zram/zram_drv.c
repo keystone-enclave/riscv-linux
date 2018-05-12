5e08182ab784cb235675c982a5b37ee477e40e44
Minchan Kim <minchan@kernel.org>
2018-05-12 10:10:06 +1000

    zram-introduce-zram-memory-tracking-update-fix-fix-fix
    fix compile warning and use zd for ssize_t by Randy's suggestion.
    
    drivers/block/zram/zram_drv.c: In function 'read_block_state':
    drivers/block/zram/zram_drv.c:674:16: warning: format '%llu' expects argument of type 'long long unsigned int', but argument 5 has type '__kernel_time_t {aka long int}' [-Wformat=]
        "%12lu %12llu.%06lu %c%c%c\n",
               ~~~~~^
               %12lu
        (unsigned long)index, ts.tv_sec,
                              ~~~~~~~~~
    Link: http://lkml.kernel.org/r/20180508104849.GA8209@rodete-desktop-imager.corp.google.com
    Signed-off-by: Minchan Kim <minchan@kernel.org>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -671,8 +671,8 @@ static ssize_t read_block_state(struct file *file, char __user *buf,
 
 		ts = ktime_to_timespec64(zram->table[index].ac_time);
 		copied = snprintf(kbuf + written, count,
-			"%12lu %12llu.%06lu %c%c%c\n",
-			(unsigned long)index, ts.tv_sec,
+			"%12zd %12lld.%06ld %c%c%c\n",
+			index, (s64)ts.tv_sec,
 			ts.tv_nsec / NSEC_PER_USEC,
 			zram_test_flag(zram, index, ZRAM_SAME) ? 's' : '.',
 			zram_test_flag(zram, index, ZRAM_WB) ? 'w' : '.',
