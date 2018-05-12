7f74cbf54d761caaa063faa189a9dabb0689ca0e
Roman Gushchin <guro@fb.com>
2018-05-12 10:10:19 +1000

    mm, oom: cgroup-aware OOM killer
    Traditionally, the OOM killer is operating on a process level.  Under oom
    conditions, it finds a process with the highest oom score and kills it.
    
    This behavior doesn't suit well the system with many running containers:
    
    1) There is no fairness between containers.  A small container with few
       large processes will be chosen over a large one with huge number of
       small processes.
    
    2) Containers often do not expect that some random process inside will
       be killed.  In many cases much safer behavior is to kill all tasks in
       the container.  Traditionally, this was implemented in userspace, but
       doing it in the kernel has some advantages, especially in a case of a
       system-wide OOM.
    
    To address these issues, the cgroup-aware OOM killer is introduced.
    
    This patch introduces the core functionality: an ability to select a
    memory cgroup as an OOM victim.  Under OOM conditions the OOM killer looks
    for the biggest leaf memory cgroup and kills the biggest task belonging to
    it.
    
    The following patches will extend this functionality to consider non-leaf
    memory cgroups as OOM victims, and also provide an ability to kill all
    tasks belonging to the victim cgroup.
    
    The root cgroup is treated as a leaf memory cgroup, so it's score is
    compared with other leaf memory cgroups.  Due to memcg statistics
    implementation a special approximation is used for estimating oom_score of
    root memory cgroup: we sum oom_score of the belonging processes (or, to be
    more precise, tasks owning their mm structures).
    
    Link: http://lkml.kernel.org/r/20171130152824.1591-4-guro@fb.com
    Signed-off-by: Roman Gushchin <guro@fb.com>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Vladimir Davydov <vdavydov.dev@gmail.com>
    Cc: Tetsuo Handa <penguin-kernel@I-love.SAKURA.ne.jp>
    Cc: David Rientjes <rientjes@google.com>
    Cc: Tejun Heo <tj@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- include/linux/memcontrol.h
+++ include/linux/memcontrol.h
@@ -35,6 +35,7 @@ struct mem_cgroup;
 struct page;
 struct mm_struct;
 struct kmem_cache;
+struct oom_control;
 
 /* Cgroup-specific page state, on top of universal node page state */
 enum memcg_stat_item {
@@ -360,6 +361,11 @@ struct mem_cgroup *mem_cgroup_from_css(struct cgroup_subsys_state *css){
 	return css ? container_of(css, struct mem_cgroup, css) : NULL;
 }
 
+static inline void mem_cgroup_put(struct mem_cgroup *memcg)
+{
+	css_put(&memcg->css);
+}
+
 #define mem_cgroup_from_counter(counter, member)	\
 	container_of(counter, struct mem_cgroup, member)
 
@@ -500,6 +506,8 @@ static inline bool task_in_memcg_oom(struct task_struct *p)
 }
 
 bool mem_cgroup_oom_synchronize(bool wait);
+
+bool mem_cgroup_select_oom_victim(struct oom_control *oc);
 
 #ifdef CONFIG_MEMCG_SWAP
 extern int do_swap_account;
@@ -821,6 +829,10 @@ static inline struct mem_cgroup *get_mem_cgroup_from_mm(struct mm_struct *mm)
 	return NULL;
 }
 
+static inline void mem_cgroup_put(struct mem_cgroup *memcg)
+{
+}
+
 static inline struct mem_cgroup *
 mem_cgroup_iter(struct mem_cgroup *root,
 		struct mem_cgroup *prev,
@@ -1012,6 +1024,11 @@ static inline void count_memcg_page_event(struct page *page,
 static inline
 void count_memcg_event_mm(struct mm_struct *mm, enum vm_event_item idx)
 {
+}
+
+static inline bool mem_cgroup_select_oom_victim(struct oom_control *oc)
+{
+	return false;
 }
 #endif /* CONFIG_MEMCG */
 
