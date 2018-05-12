c2346f5a77945a98ba165fa31d9c7def12b1dd42
Andrew Morton <akpm@linux-foundation.org>
2018-05-12 10:10:13 +1000

    mm-ksm-move-page_stable_node-from-ksmh-to-ksmc-fix
    fix SYSFS=n build
    
    Cc: Andrea Arcangeli <aarcange@redhat.com>
    Cc: Mike Rapoport <rppt@linux.vnet.ibm.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>

--- mm/ksm.c
+++ mm/ksm.c
@@ -823,11 +823,6 @@ static int unmerge_ksm_pages(struct vm_area_struct *vma,
 	return err;
 }
 
-#ifdef CONFIG_SYSFS
-/*
- * Only called through the sysfs control interface:
- */
-
 static inline struct stable_node *page_stable_node(struct page *page)
 {
 	return PageKsm(page) ? page_rmapping(page) : NULL;
@@ -839,6 +834,10 @@ static inline void set_page_stable_node(struct page *page,
 	page->mapping = (void *)((unsigned long)stable_node | PAGE_MAPPING_KSM);
 }
 
+#ifdef CONFIG_SYSFS
+/*
+ * Only called through the sysfs control interface:
+ */
 static int remove_stable_node(struct stable_node *stable_node)
 {
 	struct page *page;
