0cb7fd357aee28085fcf197368d81733640e4348
Dayeol Lee <dayeol@berkeley.edu>
2018-09-06 22:53:40 -0700

    Added Keystone Runtime [not secure yet] (#2)
    Runtime runs in S-mode and is trusted code.
    Runtime handles system calls, interrupts/exceptions inside enclave.
    Runtime is copied into each enclave and allocated in EPM.
    
    Including following commits in keystone-rt branch.
    
    * Moved page table initialize code into driver;
    * page table management (haven't debugged)
    * runtime initialization done (debugged)
    * Toy program works!

---
+++ arch/riscv/drivers/keystone-page.c
@@ -0,0 +1,153 @@
+#include "riscv64.h"
+#include "keystone-page.h"
+#include <linux/kernel.h>
+
+#define NEXT_PAGE(pa) *((vaddr_t*)pa)
+
+void init_free_pages(pg_list_t* pg_list, vaddr_t base, unsigned int count)
+{
+  unsigned int i;
+  vaddr_t cur;
+  pg_list->count = 0;
+  pg_list->head = 0;
+  pg_list->tail = 0;
+
+  cur = base;
+  for(i=0; i<count; i++)
+  {
+    put_free_page(pg_list, cur);
+    cur += RISCV_PGSIZE;
+  }
+  return;
+}
+
+vaddr_t get_free_page(pg_list_t* pg_list)
+{
+  vaddr_t free_page;
+  if(pg_list->head != 0) {
+    free_page = pg_list->head;
+    if(pg_list->head == pg_list->tail) {
+      pg_list->head = 0;
+      pg_list->tail = 0;
+    } else {
+      vaddr_t next = NEXT_PAGE(pg_list->head);
+      pg_list->head = next;
+    }
+    pg_list->count--;
+    //printm("get_free_page: free_page = 0x%llx\n", free_page);
+    return free_page;
+  }
+  //printm("out of free page\n");
+  return 0;
+}
+
+void put_free_page(pg_list_t* pg_list, vaddr_t page_addr)
+{
+  vaddr_t prev = pg_list->tail;
+  if(prev != 0) {
+    NEXT_PAGE(prev) = page_addr;
+    NEXT_PAGE(page_addr) = 0;
+    pg_list->tail = page_addr;
+  } else {
+    pg_list->head = page_addr;
+    NEXT_PAGE(page_addr) = 0;
+    pg_list->tail = page_addr;
+  }
+  pg_list->count++;
+  return;
+}
+
+void epm_init(epm_t* epm, vaddr_t base, unsigned int count)
+{
+  pte_t* t;
+  //printm("epm_init\n");
+  init_free_pages(&epm->freelist, base, count); 
+  epm->base = base;
+  epm->total = count; 
+
+  t = (pte_t*) get_free_page(&epm->freelist);
+  epm->root_page_table = t;
+  
+  return;
+}
+
+static paddr_t pte_ppn(pte_t pte)
+{
+  return pte_val(pte) >> PTE_PPN_SHIFT;
+}
+
+static paddr_t ppn(vaddr_t addr)
+{
+  return __pa(addr) >> RISCV_PGSHIFT;
+}
+
+static size_t pt_idx(vaddr_t addr, int level)
+{
+  size_t idx = addr >> (RISCV_PGLEVEL_BITS*level + RISCV_PGSHIFT);
+  return idx & ((1 << RISCV_PGLEVEL_BITS) - 1);
+}
+
+static pte_t* __ept_walk_create(epm_t* epm, vaddr_t addr);
+
+static pte_t* __ept_continue_walk_create(epm_t* epm, vaddr_t addr, pte_t* pte)
+{
+  unsigned long free_ppn = ppn(get_free_page(&epm->freelist));
+  *pte = ptd_create(free_ppn);
+  pr_info("ptd_create: ppn = %u, pte = 0x%lx\n", free_ppn,  *pte);
+  return __ept_walk_create(epm, addr);
+}
+
+static pte_t* __ept_walk_internal(epm_t* epm, vaddr_t addr, int create)
+{
+  pte_t* t = epm->root_page_table;
+  int i;
+  for (i = (VA_BITS - RISCV_PGSHIFT) / RISCV_PGLEVEL_BITS - 1; i > 0; i--) {
+    size_t idx = pt_idx(addr, i);
+    if (unlikely(!(pte_val(t[idx]) & PTE_V)))
+      return create ? __ept_continue_walk_create(epm, addr, &t[idx]) : 0;
+    t = (pte_t*) __va(pte_ppn(t[idx]) << RISCV_PGSHIFT);
+  }
+  return &t[pt_idx(addr, 0)];
+}
+
+static pte_t* __ept_walk(epm_t* epm, vaddr_t addr)
+{
+  return __ept_walk_internal(epm, addr, 0);
+}
+
+static pte_t* __ept_walk_create(epm_t* epm, vaddr_t addr)
+{
+  //printm("__ept_walk_create: addr = 0x%llx\n", addr);
+  return __ept_walk_internal(epm, addr, 1);
+}
+
+static int __ept_va_avail(epm_t* epm, vaddr_t vaddr)
+{
+  pte_t* pte = __ept_walk(epm, vaddr);
+  return pte == 0 || pte_val(*pte) == 0;
+}
+
+vaddr_t epm_alloc_page(epm_t* epm, vaddr_t addr, unsigned long flags)
+{
+  //printm("epm_alloc_page: addr = 0x%llx\n", addr);
+  pte_t* pte = __ept_walk_create(epm, addr);
+  vaddr_t page_addr = get_free_page(&epm->freelist);
+  //printm("epm_alloc_page: page_addr = 0x%llx\n", page_addr);
+  *pte = pte_create(ppn(page_addr), flags | PTE_V);
+  return page_addr;
+}
+
+vaddr_t epm_alloc_rt_page(epm_t* epm, vaddr_t addr)
+{
+  return epm_alloc_page(epm, addr, PTE_R | PTE_W | PTE_X);
+}
+
+vaddr_t epm_alloc_user_page(epm_t* epm, vaddr_t addr)
+{
+  return epm_alloc_page(epm, addr, PTE_R | PTE_X | PTE_W | PTE_U);
+}
+
+void epm_free_page(epm_t* epm, vaddr_t addr)
+{
+  /* TODO: Must Implement Quickly */
+}
--- arch/riscv/drivers/keystone.h
+++ arch/riscv/drivers/keystone.h
@@ -2,11 +2,34 @@
 #define _KEYSTONE_H_
 
 #include <linux/file.h>
+#include "keystone-page.h"
 
 #define SBI_SM_CREATE_ENCLAVE   101
 #define SBI_SM_DESTROY_ENCLAVE  102
 #define SBI_SM_COPY_TO_ENCLAVE  103
 #define SBI_SM_COPY_FROM_ENCLAVE  104
+#define SBI_SM_RUN_ENCLAVE      105
+
+/* don't want to taint asm/sbi.h, so just copied SBI_CALL and increased # args */
+#define _SBI_CALL(which, arg0, arg1, arg2, arg3, arg4, arg5) ({			\
+	register uintptr_t a0 asm ("a0") = (uintptr_t)(arg0);	\
+	register uintptr_t a1 asm ("a1") = (uintptr_t)(arg1);	\
+	register uintptr_t a2 asm ("a2") = (uintptr_t)(arg2);	\
+	register uintptr_t a3 asm ("a3") = (uintptr_t)(arg3);	\
+	register uintptr_t a4 asm ("a4") = (uintptr_t)(arg4);	\
+	register uintptr_t a5 asm ("a5") = (uintptr_t)(arg5);	\
+	register uintptr_t a7 asm ("a7") = (uintptr_t)(which);	\
+	asm volatile ("ecall"					\
+		      : "+r" (a0)				\
+		      : "r" (a1), "r" (a2), "r" (a3), "r" (a4), "r"(a5), "r" (a7)		\
+		      : "memory");				\
+	a0;							\
+})
+
+#define SBI_CALL_3(which, arg0, arg1, arg2) SBI_CALL(which,arg0, arg1, arg2)
+#define SBI_CALL_4(which, arg0, arg1, arg2, arg3) _SBI_CALL(which, arg0, arg1, arg2, arg3, 0, 0)
+#define SBI_CALL_5(which, arg0, arg1, arg2, arg3, arg4) _SBI_CALL(which, arg0, arg1, arg2, arg3, arg4, 0)
+#define SBI_CALL_6(which, arg0, arg1, arg2, arg3, arg4, arg5) _SBI_CALL(which, arg0, arg1, arg2, arg3, arg4, arg5)
 
 long keystone_ioctl(struct file* filep, unsigned int cmd, unsigned long arg);
 
@@ -17,9 +40,15 @@ struct keystone_enclave_t
   uintptr_t epm_sz;
 };
 
+// global debug functions
+void debug_dump(char* ptr, unsigned long size);
+
 // keystone enclave functions
 int keystone_create_enclave(unsigned long arg); 
 int keystone_destroy_enclave(unsigned long arg);
 int keystone_copy_to_enclave(unsigned long arg);
 int keystone_copy_from_enclave(unsigned long arg);
+// runtime loader
+int keystone_rtld_init_runtime(epm_t* epm, unsigned long epm_vaddr);
+
 #endif
