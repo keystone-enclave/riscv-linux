642985e2ab28cb5bdbfc8edbe11171dbbb22852a
David Kohlbrenner <dkohlbre@cs.ucsd.edu>
2018-10-16 14:44:03 -0700

    first pass of basic elf-loading, doesn't handle anything fancy, expects user to get entry ptr

--- arch/riscv/drivers/keystone.c
+++ arch/riscv/drivers/keystone.c
@@ -36,7 +36,6 @@ int keystone_create_enclave(unsigned long arg)
   unsigned long code_size = enclp->code_size;
   unsigned long ptr = enclp->ptr;
   unsigned long rt_offset;
-  unsigned long encl_page;
   unsigned long epm_vaddr, epm_paddr;
   unsigned long encl_stack_size = PAGE_UP(enclp->mem_size);
   // TODO: Do we want to require that requests are page sized?
@@ -47,11 +46,12 @@ int keystone_create_enclave(unsigned long arg)
   unsigned long min_pages = req_pages + 16;
   int order = ilog2(min_pages) + 1;
   int count = 0x1 << order;
+  unsigned long vaddr;
   
   //pr_info("EPM pages allocated: %d\n", count);
   epm_vaddr = __get_free_pages(GFP_HIGHUSER, order);
 
-  if(epm_vaddr == NULL){
+  if(!epm_vaddr){
     ret = -ENOMEM;
     pr_err("keystone_create_enclave: Cannot get contiguous memory for enclave. (Tried order %ul)\n", order);
     return ret;
@@ -68,34 +68,23 @@ int keystone_create_enclave(unsigned long arg)
   /* initialize runtime */
   keystone_rtld_init_runtime(epm, epm_vaddr, &rt_offset);
 
-  /* initialize enclave 
+  /* initialize enclave */
 
   /* setup enclave's stack */
-  unsigned long vaddr;
+
   for(vaddr = rt_offset - encl_stack_size; vaddr < rt_offset; vaddr+= PAGE_SIZE)
   {
     epm_alloc_user_page_noexec(epm, vaddr);
   }
   //pr_info("keystone enclave stack size: %d\n",encl_stack_size);
-  vaddr_t va;
-  unsigned int k;
 
-  // Create some code pages
-  for(va=ptr, k=0; va < ptr+code_size; va += PAGE_SIZE, k++){
+  // TODO fix code_size so that its smaller, more accurate. right now its the whole elf
   
-    encl_page = epm_alloc_user_page(epm, va);
-  
-    //pr_info("keystone_copy_to_enclave() 0x%llx <-- 0x%llx, %ld\n", encl_page, ptr, code_size);
-    
-    if(copy_from_user((void*) encl_page,
-		      (void*) va,
-		      k*PAGE_SIZE > code_size?code_size%PAGE_SIZE:PAGE_SIZE)) {
-      ret = -EFAULT;
-      goto error_free_epm;
-    }
+  ret = keystone_app_load_elf(epm, ptr, code_size);
+  if( ret != 0){
+    goto error_free_epm;
   }
-
-  //debug_dump(epm_vaddr, PAGE_SIZE*count);
+  //  debug_dump(epm_vaddr, PAGE_SIZE*count);
 
   enclp->eid = SBI_CALL_2(SBI_SM_CREATE_ENCLAVE, epm_paddr, PAGE_SIZE*count);
   if (enclp->eid < 0)
@@ -111,10 +100,100 @@ int keystone_create_enclave(unsigned long arg)
 
 error_free_epm:
   kfree(epm);
-
-error_free_pgs:
   free_pages(epm_vaddr, order);
   return ret;
+}
+
+int keystone_app_load_elf_region(epm_t* epm, unsigned long elf_usr_region, void* target_vaddr, size_t len){ 
+  unsigned long va;
+  unsigned long encl_page;
+  int k, ret = 0;
+  size_t copy_size;
+  for(va=target_vaddr, k=0; va < target_vaddr+len; va += PAGE_SIZE, k++){
+
+    encl_page = epm_alloc_user_page(epm, va);
+
+    copy_size = (k+1)*PAGE_SIZE > len ? len%PAGE_SIZE : PAGE_SIZE;
+    pr_info("Copy elf page to:%x, from: %x, sz:%i\n",
+	    encl_page, elf_usr_region + (k * PAGE_SIZE), copy_size);
+    // TODO zero out the other part of the last page
+    if(copy_from_user((void*) encl_page,
+		      (void*) elf_usr_region + (k * PAGE_SIZE),
+		      copy_size )){;
+      ret = -EFAULT;
+      break;
+    }
+  }
+ 
+  return ret;
+}
+
+int keystone_app_load_elf(epm_t* epm, unsigned long elf_usr_ptr, size_t len){
+  int retval, error, i;
+  struct elf_phdr eppnt;
+  struct elfhdr elf_ex;
+  struct elf_phdr* next_usr_phoff;
+  unsigned long vaddr;
+  unsigned long size = 0;
+  
+  error = -EFAULT;
+  
+  // TODO safety checks based on len
+
+  if(copy_from_user(&elf_ex, (void*)elf_usr_ptr, sizeof(struct elfhdr)) != 0){
+    goto out;
+  }
+
+
+  // check ELF header
+  if(memcmp(elf_ex.e_ident, ELFMAG, SELFMAG) != 0)
+    goto out;
+
+  
+  // Sanity check on elf type that its been linked as EXEC
+  if(elf_ex.e_type != ET_EXEC || !elf_check_arch(&elf_ex))
+    goto out;
+
+
+  // Get each elf_phdr in order and deal with it
+  next_usr_phoff = (void*)elf_usr_ptr + elf_ex.e_phoff;
+  for(i=0; i<elf_ex.e_phnum; i++, next_usr_phoff++) {
+
+    pr_info("loading ph %i\n",i);
+    
+    // Copy next phdr
+    if(copy_from_user(&eppnt, (void*)next_usr_phoff, sizeof(struct elf_phdr)) != 0){
+      //bad
+      continue;
+    }
+
+    // Create and copy
+    if(eppnt.p_type != PT_LOAD) {
+      pr_warn("keystone runtime includes an inconsistent program header\n");
+      continue;
+    }
+    vaddr = eppnt.p_vaddr;
+    //vaddr sanity check?
+    size = eppnt.p_filesz;
+    pr_info("loading vaddr: %x, sz:%i\n",vaddr,size);
+
+
+    retval = keystone_app_load_elf_region(epm,
+					  elf_usr_ptr + eppnt.p_offset,
+					  (void*)vaddr,
+					  size);
+    if(retval != 0){
+      error = retval;
+      break;
+    }
+
+  }
+
+  error = 0;
+
+  out:
+    return error;
+  
 }
 
 int keystone_destroy_enclave(unsigned long arg)
