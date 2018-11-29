4b8f5cb373305e05181c2f4cf2518d7b2033d2a9
David Kohlbrenner <dkohlbre@cs.ucsd.edu>
2018-11-28 19:16:24 -0800

    Added licensing to keystone driver files

--- arch/riscv/drivers/keystone-ioctl.c
+++ arch/riscv/drivers/keystone-ioctl.c
@@ -1,3 +1,7 @@
+//******************************************************************************
+// Copyright (c) 2018, The Regents of the University of California (Regents).
+// All Rights Reserved. See LICENSE for license details.
+//------------------------------------------------------------------------------
 #include "keystone.h"
 #include "keystone-sbi-arg.h"
 #include "keystone_user.h"
